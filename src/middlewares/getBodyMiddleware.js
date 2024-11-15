b384a25199bf82b1f48d76de431a7b2581d64882
Marcio gabriel Mengali <marciogabrielmengali@MacBook-Air-de-Marcio.local>
2024-11-15 17:12:25 -0300

    feat: adding middlewares

---
+++ src/middlewares/getBodyMiddleware.js
@@ -0,0 +1,14 @@
+export async function getRequestBody(req, res) {
+  if (req.method === "POST") {
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    try {
+      req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch (error) {
+      console.log(`Error on parse body`, error);
+      req.body = null;
+    }
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,28 +1,36 @@
 import http from "node:http";
+import { getRequestBody } from "./middlewares/getBodyMiddleware.js";
+import { setResponseHeaders } from "./middlewares/setResponseHeadersMiddleware.js";
 
 const contacts = [];
 const CONTANCT_URL = "/contact";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   console.log(`Method: ${method} :: Url: ${url}`);
+
+  await getRequestBody(req, res);
+  setResponseHeaders(req, res);
+
   if (method === "GET" && url === CONTANCT_URL) {
     const response = JSON.stringify(contacts);
-    return res
-      .setHeader("Content-Type", "application/json")
-      .end(response);
+    return res.setHeader("Content-Type", "application/json").end(response);
   }
   if (method === "POST" && url === CONTANCT_URL) {
+    if (!req.body) {
+      return res.writeHead(400).end(JSON.stringify({ error: "Error on body" }));
+    }
+    const body = req.body
     const data = {
       id: 1,
-      name: "marcio",
-      mobilePhone: "+5519900786542",
-      email: "marcio@example.com",
+      name: body.name,
+      mobilePhone: body.mobilePhone,
+      email: body.email,
     };
     contacts.push(data);
-    return res.writeHead(201).end()
+    return res.writeHead(201).end();
   }
-  return res.writeHead(404).end('Not Found')
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
